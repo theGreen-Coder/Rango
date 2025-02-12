abda05ad13544a35d51a473c11682d07348b1301
Green Code <109465400+theGreen-Coder@users.noreply.github.com>
2025-02-12 19:35:32 +0000

    Chapter 7 Done

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128 
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
